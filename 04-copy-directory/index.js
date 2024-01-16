93b7b84e4d886cd12dbba5d616a527f2c91d5d56
gryzun33 <yakushevao777@gmail.com>
2024-01-16 23:34:13 +0300

    fix: fix solution of 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,24 +1,28 @@
 const fs = require('fs');
 const path = require('path');
 
-const pathToOldFolder = path.join(__dirname, 'files');
+function copyDir() {
+  const pathToOldFolder = path.join(__dirname, 'files');
 
-const pathToNewFolder = path.join(__dirname, 'files-copy');
+  const pathToNewFolder = path.join(__dirname, 'files-copy');
 
-fs.mkdir(pathToNewFolder, { recursive: true }, (err) => {
-  if (err) throw err;
-
-  fs.readdir(pathToOldFolder, { withFileTypes: true }, (err, files) => {
+  fs.mkdir(pathToNewFolder, { recursive: true }, (err) => {
     if (err) throw err;
 
-    files.forEach((obj) => {
-      if (obj.isFile()) {
-        const pathToOldFile = path.join(pathToOldFolder, obj.name);
-        const pathToNewFile = path.join(pathToNewFolder, obj.name);
-        fs.copyFile(pathToOldFile, pathToNewFile, (err) => {
-          if (err) throw err;
-        });
-      }
+    fs.readdir(pathToOldFolder, { withFileTypes: true }, (err, files) => {
+      if (err) throw err;
+
+      files.forEach((obj) => {
+        if (obj.isFile()) {
+          const pathToOldFile = path.join(pathToOldFolder, obj.name);
+          const pathToNewFile = path.join(pathToNewFolder, obj.name);
+          fs.copyFile(pathToOldFile, pathToNewFile, (err) => {
+            if (err) throw err;
+          });
+        }
+      });
     });
   });
-});
+}
+
+copyDir();
